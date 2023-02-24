7f7199dfe9ad0e4a9727ca0648350a1b4a0362a8
lucas <lucas@REGNUM>
2023-02-24 17:08:42 +0100

    Dodaj pliki projektów.

---
+++ EvtxToES/EvtxToES.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Net;
+using System.Xml;
+using System.IO;
+using Newtonsoft.Json;
+using RestSharp;
+
+
+#pragma warning disable IDE1006
+
+namespace EvtxToES
+{
+    public static class EventManager
+    {
+        public enum Conf
+        {
+            File,
+            Url
+        }
+        public class Rule
+        {
+            public string xpath { get; set; }
+            public string source { get; set; }
+        }
+        public class Config
+        {
+            public Rule[] rules { get; set; }
+            public string address { get; set; }
+            public int refresh_freq { get; set; }
+            public bool ssl_validation { get; set; }
+
+        }
+        public static string GetEvents(string logName, string query)
+        {
+            List<XmlDocument> events = new List<XmlDocument>();
+            EventLogQuery eventQuery = new EventLogQuery(logName, PathType.LogName, query);
+            try
+            {
+                EventLogReader eventReader = new EventLogReader(eventQuery);
+                for (EventRecord eventDetails = eventReader.ReadEvent(); eventDetails != null; eventDetails = eventReader.ReadEvent())
+                {
+                    var eventdata = eventDetails.ToXml();
+                    XmlDocument doc = new XmlDocument();
+                    doc.LoadXml(eventdata);
+                    events.Add(doc);
+                }
+                string jsonstring = JsonConvert.SerializeObject(events, Newtonsoft.Json.Formatting.Indented);
+                return jsonstring;
+            }
+            catch (Exception e)
+            {
+                var hostname = Environment.MachineName;
+                var message = e.Message;
+                var domainname = System.Net.NetworkInformation.IPGlobalProperties.GetIPGlobalProperties().DomainName;
+                string timestamp = GetTimestamp(DateTime.UtcNow);
+                string errormessage = String.Format("<?xml version=\"1.0\"?><Exception><ExceptionData><Source>EvtxToES</Source><Message>{0}</Message><Timestamp>{1}</Timestamp><Host>{2}</Host><Domain>{3}</Domain></ExceptionData></Exception>", message, timestamp, hostname, domainname);
+                XmlDocument doc = new XmlDocument();
+                doc.LoadXml(errormessage);
+                string jsonstring = JsonConvert.SerializeXmlNode(doc, Newtonsoft.Json.Formatting.Indented);
+                return jsonstring;
+
+            }
+        }
+        public static string GetTimestamp(DateTime value)
+        {
+            return value.ToString("yyyy-MM-ddTHH:mm:ss");
+        }
+        public static void SendToLogstash(string logs, string url, bool selfsigned)
+        {
+            var options = new RestClientOptions(url);
+            if (selfsigned == false)
+            {
+                options.RemoteCertificateValidationCallback = (sender, cert, chain, sslPolicyErrors) => true;
+            }
+            var client = new RestSharp.RestClient(options);
+            var request = new RestRequest("/", Method.Post)
+                .AddBody(logs);
+            request.RequestFormat = DataFormat.Json;
+            client.Execute(request);
+        }
+    }
+}
---
+++ EvtxToESSvc/EvtxToEsSvc.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Diagnostics;
+using System.ServiceProcess;
+using EvtxToES;
+using System.Timers;
+using RestSharp;
+using Newtonsoft.Json;
+using System.IO;
+
+#pragma warning disable IDE0044
+#pragma warning disable IDE0079
+
+namespace EvtxToESSvc
+{
+    
+    public partial class EvtxToEsSvc : ServiceBase
+    {
+        
+        private EvtxToES.EventManager.Config currentConfig;
+        private EventLog ServiceEvtx;
+        public EvtxToEsSvc(string[] args)
+        {
+            InitializeComponent(); 
+            this.ServiceEvtx = new System.Diagnostics.EventLog();
+            if (!System.Diagnostics.EventLog.SourceExists("EvtxToESSvc"))
+            {
+                System.Diagnostics.EventLog.CreateEventSource(
+                    "EvtxToESSvc", "EvtxToESSvcLog");
+            }
+            this.ServiceEvtx.Source = "EvtxToESSvc";
+            this.ServiceEvtx.Log = "EvtxToESSvcLog";
+            this.currentConfig = new EvtxToES.EventManager.Config();
+        }
+        protected override void OnStart(string[] args)
+        {
+            string[] arguments = Environment.GetCommandLineArgs();
+            string argholder = "";
+            foreach(string arg in arguments)
+            {
+                argholder = argholder + arg + '\r';
+            }
+            argholder += arguments.Length.ToString();
+            this.ServiceEvtx.WriteEntry(argholder);
+
+
+            /*Check the arguments*/
+            if (arguments.Length < 3 || arguments.Length > 4)
+            {
+                this.ServiceEvtx.WriteEntry("Service started with wrong number of parameters. " +
+                    "Provide paramaters like: type(url|file), path(http://example.com:5000|C:\\Users\\JohnDoe\\config.json), ssl_validation(selfisnged|)");
+                this.Stop();
+            }
+            else if(arguments.Length == 4)
+            {
+                if (arguments[3] == "selfsigned")
+                {
+                    //ServicePointManager.ServerCertificateValidationCallback += (sender, cert, chain, sslPolicyErrors) => true;
+                }
+            }
+            if (arguments[1] != "url" && arguments[1] != "file")
+            {
+                this.ServiceEvtx.WriteEntry("You have provided wrong config source type. Enter (file|url) as a first argument.");
+                this.Stop();
+            }
+            
+            /*Logging start with parameters*/
+            string arguments_string = "";
+            foreach(string arg in arguments)
+            {
+                arguments_string += arg;
+                arguments_string += '\r';
+            }
+            string url = arguments[2];
+            this.ServiceEvtx.WriteEntry("Service started with parameters: \r" + arguments_string);
+
+
+
+            try
+            {
+                /*Strings for logging*/
+                string logtext = "Application Loaded with rules:\r";
+                string configString;
+
+                /*Getting config from url*/
+                if (arguments[1] == "url")
+                {
+                    var options = new RestClientOptions(url);
+                    if (arguments.Length == 4)
+                    {
+                        if (arguments[3] == "selfsigned")
+                        {
+                            options.RemoteCertificateValidationCallback = (sender, cert, chain, sslPolicyErrors) => true;
+                        }
+                    }
+                    var client = new RestClient(options);
+                    var request = new RestRequest("/", Method.Get);
+                    RestResponse response = client.Execute(request);
+                    configString = response.Content;
+                }
+                
+            
+                
+
+                /*Getting config from file*/
+                else
+                {
+                    configString = File.ReadAllText(arguments[2]);
+                }
+
+                /*Logging the rules to the evtx*/
+                ServiceEvtx.WriteEntry("Service loaded logging policy: \r" + configString);
+                var currentConfig = JsonConvert.DeserializeObject<EventManager.Config>(configString);
+                foreach (EventManager.Rule rule in currentConfig.rules)
+                {
+                    logtext = logtext + rule.xpath + "\r";
+                }
+                ServiceEvtx.WriteEntry("Service downloaded logging policy: \r" + logtext);
+
+                /*Setting the timer*/
+                Timer timer = new Timer() { Interval = Convert.ToDouble(currentConfig.refresh_freq) };
+                timer.Elapsed += new ElapsedEventHandler(this.OnTimer);
+                timer.Start();
+                ServiceEvtx.WriteEntry("Service sets it's timer to: \r" + timer.Interval);
+                
+                /*Saving the config for global use*/
+                this.currentConfig = currentConfig;
+                }
+                /*Catching general WTF's to Event Log*/
+                catch (Exception e)
+                {
+                    ServiceEvtx.WriteEntry(e.Message + e.StackTrace);
+                }
+        }
+
+        protected override void OnStop()
+        {
+            //Stop the Service and Log the poweroff
+            ServiceEvtx.WriteEntry("Service is about to stop.");
+        }
+        public void OnTimer(object sender, ElapsedEventArgs args)
+        {
+            
+            foreach(EvtxToES.EventManager.Rule rules in currentConfig.rules)
+            {
+                var ruleString = rules.xpath;
+                var rulesource = rules.source;
+                string outputJson = EvtxToES.EventManager.GetEvents(rulesource, ruleString);
+                EventManager.SendToLogstash(outputJson, currentConfig.address, currentConfig.ssl_validation);
+            }
+        }
+    }
+}
